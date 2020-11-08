e734c3e2934e6f527fe4b2aaedbd6c982b3f66f3
shahryar-cymk <shahryar.r101@gmail.com>
2020-11-08 07:37:28 +0500

    Example Modified

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,133 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:provider/provider.dart';
+// import 'package:haider_app/screens/business_overview_screen.dart';
+// import 'package:haider_app/screens/splash_screen.dart';
+
+// import './screens/login_screen.dart';
+
+// import './providers/auth.dart';
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//         providers: [
+//           ChangeNotifierProvider(
+//             create: (context) => Auth(),
+//           ),
+//         ],
+//         child: Consumer<Auth>(builder: (ctx, auth, _) {
+//           // print(auth.isAuth);
+//           return MaterialApp(
+//             debugShowCheckedModeBanner: false,
+//             title: '''Haider's App''',
+//             theme: ThemeData(
+//               scaffoldBackgroundColor: Colors.white,
+//               primaryColor: Color.fromRGBO(124, 116, 146, 1),
+//               buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+//               appBarTheme: AppBarTheme(
+//                   color: Colors.white,
+//                   elevation: 0.0,
+//                   iconTheme:
+//                       IconThemeData(color: Color.fromRGBO(124, 116, 146, 1))),
+//             ),
+//             home: auth.isAuth
+//                 ? BusinessOverViewScreen()
+//                 : FutureBuilder(
+//                     builder: (context, authResultSnapshot) {
+//                       print(auth.isAuth);
+//                       return authResultSnapshot.connectionState ==
+//                               ConnectionState.waiting
+//                           ? SplashScreen()
+//                           : LoginScreen();
+//                     },
+//                     future: auth.tryAutoLogin(),
+//                   ),
+//             routes: {
+//               LoginScreen.routeName: (ctx) => LoginScreen(),
+//               // RegisterScreen.routeName: (ctx) => RegisterScreen(),
+//               BusinessOverViewScreen.routeName: (ctx) =>
+//                   BusinessOverViewScreen()
+//             },
+//           );
+//         }));
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:sms_maintained/sms.dart';
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+//   runApp(
+//     MaterialApp(
+//       home: SendSmsScreen(),
+//     ),
+//   );
+// }
+
+// class SendSmsScreen extends StatelessWidget {
+//   final SmsSender sender = SmsSender();
+//   final SmsMessage message = SmsMessage('03009640742', '_body');
+//   final List<SmsMessage> smsList = [
+//     SmsMessage('03009750742', 'Random'),
+//     SmsMessage('03008750742', 'Kaleem'),
+//     SmsMessage('03056750742', 'Shahryar'),
+//     SmsMessage('03127750742', 'Shahryar Zong')
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(),
+//         body: ListView.builder(
+//           itemCount: smsList.length,
+//           itemBuilder: (context, index) {
+//             return SingleChildScrollView(
+//               child: Column(
+//                 children: [
+//                   IconButton(
+//                     icon: Icon(Icons.send),
+//                     onPressed: () {
+//                       sender.sendSms(smsList[index]);
+//                     },
+//                   ),
+//                   StreamBuilder<SmsMessageState>(
+//                     key: GlobalKey(debugLabel: 'String $index'),
+//                     // initialData: SmsMessageState.None,
+//                     builder: (context, snapshot) {
+//                       if (snapshot.data == SmsMessageState.Sent) {
+//                         return Text('Message Sent');
+//                       }
+//                       if (snapshot.data == SmsMessageState.Delivered) {
+//                         return Text('Message Delivered');
+//                       }
+//                       if (snapshot.data == SmsMessageState.Sending) {
+//                         return Text('Sending');
+//                       }
+//                       if (snapshot.data == SmsMessageState.Fail)
+//                         return Text('Sending Failed');
+//                       return Text('Error');
+//                     },
+//                     stream: smsList[index].onStateChanged,
+//                   ),
+//                 ],
+//               ),
+//             );
+//           },
+//         ));
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:woosignal/models/response/products.dart';
+import 'package:woosignal/models/response/customer_batch.dart';
 import 'package:woosignal/woosignal.dart';
 
 void main() => runApp(MyApp());
@@ -31,16 +159,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _incrementCounter() async {
     // CONFIG FOR WOOSIGNAL
-    var wsConfig = {"appKey": "your app key", "debugMode": true};
+    var wsConfig = {
+      "appKey":
+          "app_affb6434339b34443a297c2e40a3edab7102137e6d67de9abfe612b749bd",
+      "debugMode": true
+    };
 
     // CREATING AN INSTANCE
     WooSignal wooSignal = await WooSignal.getInstance(config: wsConfig);
 
-    List<Product> products = await wooSignal.getProducts();
-    print(products[0].name);
+    Customers coupons = await wooSignal.retrieveCustomer(id: 2);
+    print(coupons.email);
 
     setState(() {
-      _productName = products[0].name;
+      _productName = coupons.email;
     });
   }
 
