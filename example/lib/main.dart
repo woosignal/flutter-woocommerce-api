cdc7c89d54d98e43d831e06429374cc5d13b9c57
Shahryar <shahryar.r101@gmail.com>
2020-11-15 20:10:11 +0500

    Dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:woosignal/models/response/setting_option_batch.dart';
+import 'package:woosignal/models/response/products.dart';
 import 'package:woosignal/woosignal.dart';
 
 void main() => runApp(MyApp());
@@ -31,36 +31,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _incrementCounter() async {
     // CONFIG FOR WOOSIGNAL
-    var wsConfig = {
-      "appKey":
-          "app_affb6434339b34443a297c2e40a3edab7102137e6d67de9abfe612b749bd",
-      "debugMode": true
-    };
+    var wsConfig = {"appKey": "your app key", "debugMode": true};
 
     // CREATING AN INSTANCE
     WooSignal wooSignal = await WooSignal.getInstance(config: wsConfig);
-    SettingOptionBatch couponBatch = await wooSignal.batchSettingOptions(
-      data: {
-        "create": [
-          {
-            "regular_price": "10.00",
-            "attributes": [
-              {
-                "id": "6",
-                "option": "Blue",
-              }
-            ]
-          }
-        ],
-        "update": [
-          {"id": "733", "minimum_amount": "10.00"}
-        ],
-        "delete": [732],
-      },
-    );
+
+    List<Product> products = await wooSignal.getProducts();
+    print(products[0].name);
 
     setState(() {
-      _productName = coupons.description;
+      _productName = products[0].name;
     });
   }
 
